f2af397bde0e166a5ac581fb872a5923fb68961d
Gabriel018 <duke.comandos@gmail.com>
2022-05-23 15:06:54 -0300

    .

---
+++ Breakout/script.js
@@ -0,0 +1,8 @@
+const grid = document.querySelector('.grid')
+const block = document.createElement('div')
+
+
+block.classList.add('block')
+block.style.left = '200px'
+block.style.bottom = '200px'
+grid.appendChild(block)
