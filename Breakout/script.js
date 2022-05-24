4d3ef8acc7ce91874c01fffd37f83d8e29ba78fb
Gabriel018 <duke.comandos@gmail.com>
2022-05-24 14:57:18 -0300

    1.1

--- Breakout/script.js
+++ Breakout/script.js
@@ -1,8 +1,36 @@
 const grid = document.querySelector('.grid')
 const block = document.createElement('div')
+const blockWidth = 100
+const blockHight = 20
 
 
-block.classList.add('block')
-block.style.left = '200px'
-block.style.bottom = '200px'
-grid.appendChild(block)
+class Block {
+    constructor(x,y){
+        this.bottomLeft = [x , y]
+        this.bottomRight = [x + blockWidth, y]
+        this.topleft= [x , y + blockHight]
+        this.topRight = [x + blockWidth, y + blockHight]
+        
+        
+
+    }
+
+}
+
+const blocks = [
+    new Block(20,280)
+]
+ 
+function addBlocks(){
+
+    for (let i = 0; i < blocks.length; i++)
+    {   
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.style.left = blocks[i].bottomLeft[0] + 'px'
+        block.style.bottom = blocks[i.bottomRight[1]] + 'px'
+        grid.appendChild(block)
+    }
+
+}
+addBlocks()
