4c2ca8d5af98badd3a7ecbe7279b7f7b3ff90a83
Gabriel018 <duke.comandos@gmail.com>
2022-05-30 14:46:08 -0300

    1,1

--- Breakout/script.js
+++ Breakout/script.js
@@ -2,9 +2,12 @@ const grid = document.querySelector('.grid')
 const block = document.createElement('div')
 const blockWidth = 100
 const blockHight = 20
+const bord = 500
 
 const StartPosition = [270,10]
 let  Position = StartPosition
+
+const BallStart = [310,30]
 
 
 class Block {
@@ -52,31 +55,32 @@ user.style.bottom = StartPosition[1] + 'px'
 grid.appendChild(user)
 
 
+
 function Draw(){
     user.style.left = StartPosition[0] + 'px'
     user.style.bottom = StartPosition[1] + 'px'
 }
 
-function MoverL(e){
+function Mover(e){
     switch(e.key){
         case 'ArrowLeft':
             StartPosition[0] -= 10
             Draw()
             break;
-    }
-}
-
-function MoverR(e){
-    switch(e.key){
         case 'ArrowRight':
-            StartPosition[0] += 10
-            Draw()
-            break;
+            if (StartPosition[0] < bord){
+                StartPosition[0] += 10
+                Draw()
+            }
+                break;    
     }
 }
 
 
+document.addEventListener('keydown',Mover)
 
-
-document.addEventListener('keydown',MoverL)
-document.addEventListener('keydown',MoverR)
+const ball = document.createElement('div')
+ball. classList.add('ball')
+ball.style.left = BallStart[0] + 'px'
+ball.style.bottom = BallStart[1] + 'px'
+grid.appendChild(ball)
