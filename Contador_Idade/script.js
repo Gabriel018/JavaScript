0363ca16bb3a2122413bf7c13a0ce08f326fd45e
Gabriel018 <duke.comandos@gmail.com>
2022-06-10 17:28:28 -0300

    .

--- Contador_Idade/script.js
+++ Contador_Idade/script.js
@@ -4,8 +4,9 @@ function verificar(){
    var fano = document.getElementById('ano')
    var res = document.getElementById('res')
    var fsexo = document.getElementsByName('sex')
-   
+   var idade = atual_ano - Number(fano.value)
    var genero = ''
+
    if( fano.value.length  == 0 || Number(fano.value) > atual_ano){
     alert('Algo deu errado')
    }
@@ -17,10 +18,11 @@ function verificar(){
 
     (fsexo[1]) 
       genero = 'Feminino'
-
    }
-  
-    var idade = atual_ano - Number(fano.value)
-    res.innerHTML = ( `Seu genero e ${genero} e sua idade e: ${idade} anos`);
-
+   if ( idade <= 15) 
+    res.innerHTML = ( `Voce e uma criança ${genero} e sua idade e: ${idade} anos`);
+    if ( idade >= 15) 
+    res.innerHTML = ( `Voce e uma jovem ${genero} e sua idade e: ${idade} anos`);
+    if ( idade >= 60) 
+    res.innerHTML = ( `Voce e uma idoso ${genero} e sua idade e: ${idade} anos`);
 }
