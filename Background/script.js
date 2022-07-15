3947178a72b370f830a2d38d221d645b2c41cd77
Gabriel018 <duke.comandos@gmail.com>
2022-07-14 22:21:36 -0300

    .

--- Background/script.js
+++ Background/script.js
@@ -1,5 +1,16 @@
-const cores  = ["red",'blue',"grey"]
+const cores  = ["red","blue","grey",'pink']
 
 const btn = document.getElementById('btn')
 
 const cor = document.querySelector('.color')
+
+
+btn.addEventListener('click',function(){
+  const randomNumber = getColor();
+  document.body.style.backgroundColor = cores[randomNumber]
+  cor.textContent = cores[randomNumber]
+});
+
+function getColor (){
+    return Math.floor(Math.random()* cores.length)
+}
