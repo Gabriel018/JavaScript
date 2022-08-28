081d8fa99eb7542ac11e8e66a8929ac03e31a232
Gabriel018 <duke.comandos@gmail.com>
2022-08-28 19:58:55 -0300

    1.1

--- Menu_Herois/script.js
+++ Menu_Herois/script.js
@@ -2,42 +2,66 @@
 const Herois = [{
     id: 1,
     nome: "Gult",
-    classe: "Guerreiro",
+    Categoria: "Guerreiro",
     img: "img/guerreiro.jpg"
 
 },
 {
     id: 2,
     nome: "Mago_da_escuridao" ,
-    classe: "Mago",
+    Categoria: "Mago",
     img: "img/mago.jpg"
 
 },
 {
     id: 1,
     nome: "Vult",
-    classe: "Arqueiro",
+    Categoria: "Arqueiro",
     img: "img/arqueiro.jpg"
 
 }
 ]
 
 const Heroi_Section = document.querySelector(".Heroi-Container")
-const Btn = document.querySelector(".btn")
+const Btn = document.querySelectorAll(".btn")
 
 window.addEventListener("DOMContentLoaded", function(){
-    let DisplayMenu = Herois.map(function(item){
-     
-    return `<div class="Heroi-Container">
-    <div class="Heroi">
-        <img src=${item.img} alt="">
-        <p>Nome: ${item.nome}</h2>
-        <P>Classe:${item.classe}</P>
-        <p>idade:23</p>
-    </div>`
-
+   DisplayMenuItems(Herois)   
 
     })
-    Heroi_Section.innerHTML = DisplayMenu
+Btn.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        const Categoria = e.currentTarget.dataset.id
+        const MenuCategoria = Herois.filter(function(MenuItens){
+        if(MenuItens.Categoria === Categoria){
+            return MenuItens 
+        }
+            
+        })
+        if(Categoria === "Todos"){
+        DisplayMenuItems(Herois)
+        }
+        else{
+        DisplayMenuItems(MenuCategoria)  
+        }
+       
+     console.log(Categoria)
+    })
 })
+
+function DisplayMenuItems(MenuItens){
+    let DisplayMenu = MenuItens.map(function(item){
+     
+        return `<div class="Heroi-Container">
+        <div class="Heroi">
+            <img src=${item.img} alt="">
+            <p>Nome: ${item.nome}</h2>
+            <P>Categoria:${item.Categoria}</P>
+            <p>idade:23</p>
+        </div>`
+    
+})    
+    Heroi_Section.innerHTML = DisplayMenu
+}
+
 
