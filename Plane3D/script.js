0a525fe958c658e482b6479467551aa08ec802b2
Gabriel018 <duke.comandos@gmail.com>
2022-09-12 22:32:14 -0300

    .

---
+++ Plane3D/script.js
@@ -0,0 +1,23 @@
+
+
+var scene = new THREE.Scene();
+scene.background = new THREE.Color(0xA9A9A9)
+var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+
+
+const loader = new GLTFLoader();
+
+loader.load( 'path/to/model.glb', function ( gltf ) {
+
+	scene.add( gltf.scene );
+
+}, undefined, function ( error ) {
+
+	console.error( error );
+
+} );
