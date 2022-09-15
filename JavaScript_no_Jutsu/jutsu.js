6eda957f74c989da62209f45971139641bd1ef3d
Gabriel018 <duke.comandos@gmail.com>
2022-09-14 22:57:48 -0300

    1m1

---
+++ JavaScript_no_Jutsu/jutsu.js
@@ -0,0 +1,46 @@
+const selos = [{
+    id: 1,
+    nome: "Porco",
+    img : "img/Porco.jpg"
+
+},
+{
+    id: 2,
+    nome: "Galo",
+    img : "img/Galo.jpg"
+
+},
+{
+    id: 3,
+    nome: "Rato",
+    img : "img/Rato.jpg"
+
+},
+{
+    id: 4,
+    nome: "Cachorro",
+    img: "img/Cachorro.jpg"
+
+},
+]
+
+const gridcard = document.querySelector(".grid")
+
+window.addEventListener("DOMContentLoaded", function(){
+DisplayMenuItems(selos)
+})
+
+
+function DisplayMenuItems(MenuItens){
+  let  DisplayMenu = MenuItens.map(function(item){
+        console.log(item)
+    
+    return `<div class="grid">
+    <img src="${item.img}">
+     </div>`
+
+    })
+    console.log(DisplayMenu)
+    gridcard.innerHTML = DisplayMenu
+    
+}
