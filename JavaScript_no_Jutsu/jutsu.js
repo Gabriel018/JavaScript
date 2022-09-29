fb98f7b1f725966bdb11cd9c3abfffff241c6e55
Gabriel018 <duke.comandos@gmail.com>
2022-09-28 22:20:12 -0300

    1.1

--- JavaScript_no_Jutsu/jutsu.js
+++ JavaScript_no_Jutsu/jutsu.js
@@ -1,13 +1,19 @@
+
+
 const btn = document.querySelectorAll(".card")
 
 const juju = document.querySelector(".jutsu")
+const modal = document.getElementById("mymodal")
 
 function mostrar(clicked){
+   var audio = new Audio('img/itachi_Jutsu.mp3')
+   
    console.log(clicked)
-   if(clicked === "Dog" && "Galo"){
-    juju.classList.remove("jutsu")
-    
+   if(clicked === "Dog" && clicked === "Galo" ){
+    audio.play()  
+    modal.style.display="block"
    }
+   
     }
 
    
